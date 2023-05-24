822801f0b3472646c4233603cbb9795993f15d57
Xeon0X <xeon0x.gaming@gmail.com>
2023-05-24 20:43:03 +0200

    Propagation working!

--- Terraformer/dataAcquisition/Block.py
+++ Terraformer/dataAcquisition/Block.py
@@ -1,6 +1,9 @@
 from gdpc import Editor, Block, geometry
+from gdpc.lookup import *
 import numpy as np
 import math
+
+SOLID_NATURAL_BLOCKS = SOILS | STONES | ORES | LIQUIDS
 
 class Block:
     def __init__(self, coordinates:tuple, name:str):
@@ -15,14 +18,17 @@ class Block:
             self.neighbors.append(neighbor)
 
     def isSurface(self):
-        if str(self.name) != 'minecraft:air':
-            for neighbor in self.neighbors:
-                if str(neighbor.name) == 'minecraft:air':
-                    self.surface = True
-                    break
-            if len(neighbor) != 0:
+        if self.surface == None:
+            if str(self.name) in SOLID_NATURAL_BLOCKS:
+                for neighbor in self.neighbors:
+                    if str(neighbor.name) not in SOLID_NATURAL_BLOCKS:
+                        self.surface = True
+                        return True
+                if len(self.neighbors) != 0:
+                    self.surface = False
+                    return False
+            else:
                 self.surface = False
-
-from gdpc.lookup import *
-
-print(AIRS, FIRES, FLOWERS, GRASS_PLANTS, CROPS, SAPLINGS, FOLIAGE, )
+                return False
+        else:
+            return self.surface
--- Terraformer/dataAcquisition/World.py
+++ Terraformer/dataAcquisition/World.py
@@ -1,4 +1,5 @@
-from gdpc import Editor, Block, geometry
+from gdpc import Editor, geometry
+from gdpc import Block as place
 import numpy as np
 import math
 
@@ -32,31 +33,34 @@ class World:
         for block in blocks:
             if self.isInVolume(block.coordinates):
                 self.volume[block.coordinates[0] - self.coordinates_min[0]][block.coordinates[1] - self.coordinates_min[1]][block.coordinates[2] - self.coordinates_min[2]] = block
-            else:
-                raise "Block not in volume"
 
     def getBlockFromCoordinates(self, coordinates):
+            if self.volume[coordinates[0] - self.coordinates_min[0]][coordinates[1] - self.coordinates_min[1]][coordinates[2] - self.coordinates_min[2]] == None:
+                self.volume[coordinates[0] - self.coordinates_min[0]][coordinates[1] - self.coordinates_min[1]][coordinates[2] - self.coordinates_min[2]] = Block((coordinates[0], coordinates[1], coordinates[2]), editor.getBlock((coordinates[0], coordinates[1], coordinates[2])).id)
+            
             return self.volume[coordinates[0] - self.coordinates_min[0]][coordinates[1] - self.coordinates_min[1]][coordinates[2] - self.coordinates_min[2]]
 
 
     def getNeighbors(self, Block):
         for i in range(-1, 2):
-            if i != 0:
-                coordinates = (Block.coordinates[0]+i, Block.coordinates[1], Block.coordinates[2])
-                if self.isInVolume(coordinates):
-                    Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
+            for j in range(-1, 2):
+                for k in range(-1, 2):
+                    if not (i == 0 and j == 0 and k == 0):
+                        coordinates = (Block.coordinates[0]+i, Block.coordinates[1]+j, Block.coordinates[2]+k)
+                        if self.isInVolume(coordinates):
+                            Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
         
-        for j in range(-1, 2):
-            if j != 0:
-                coordinates = (Block.coordinates[0], Block.coordinates[1]+j, Block.coordinates[2])
-                if self.isInVolume(coordinates):
-                    Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
+        # for j in range(-1, 2):
+        #     if j != 0:
+        #         coordinates = (Block.coordinates[0], Block.coordinates[1]+j, Block.coordinates[2])
+        #         if self.isInVolume(coordinates):
+        #             Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
         
-        for k in range(-1, 2):
-            if k != 0:
-                coordinates = (Block.coordinates[0], Block.coordinates[1], Block.coordinates[2]+k)
-                if self.isInVolume(coordinates):
-                    Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
+        # for k in range(-1, 2):
+        #     if k != 0:
+        #         coordinates = (Block.coordinates[0], Block.coordinates[1], Block.coordinates[2]+k)
+        #         if self.isInVolume(coordinates):
+        #             Block.addNeighbors([self.getBlockFromCoordinates(coordinates)])
 
 
     def setVolume(self):
@@ -67,25 +71,33 @@ class World:
                 for z in range(self.coordinates_min[2], self.coordinates_max[2]+1):
                     #self.volume[i][j][k] = Block((x, y, z), editor.getBlock((x, y, z)))
                     print(x, y, z)
-                    self.addBlocks([Block((x, y, z), editor.getBlock((x, y, z)))])
+                    self.addBlocks([Block((x, y, z), editor.getBlock((x, y, z)).id)])
 
     def propagate(self, coordinates, scanned=[]):
+        i = 0
+        editor = Editor(buffering=True)
         if self.isInVolume(coordinates):
             Block = self.getBlockFromCoordinates(coordinates)
             self.getNeighbors(Block)
             for neighbor in Block.neighbors:
                 if neighbor not in scanned:
                     scanned.append(neighbor)
-                    if str(neighbor.name) != 'minecraft:air':
+                    self.getNeighbors(neighbor)
+                    if neighbor.isSurface():
+                        print(i)
+                        i += 1
+                        editor.placeBlock(neighbor.coordinates, place("minecraft:glass"))
+                        print(neighbor.coordinates)
                         self.propagate(neighbor.coordinates, scanned)
+                        
 
 w = World()
-print(len(w.volume))
-w.setVolume()
-print(w.volume[0][0][0].name)
-print("done")
-
-w.propagate(w.volume[0][0][0].coordinates)
-
-for i in range(0, 6):
-    print(w.volume[1][1][1].neighbors[i].name)
+#print(len(w.volume))
+#w.setVolume()
+#print(w.volume[0][0][0].name)
+#print("done")
+editor = Editor(buffering=False)
+import sys
+sys.setrecursionlimit(w.length_x * w.length_y * w.length_z)
+w.propagate((2182, 75, 1060))
+print("fine")

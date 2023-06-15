00307ffcca90c15ec868924414ef195db59ae001
Xeon0X <xeon0x.gaming@gmail.com>
2023-06-15 23:51:05 +0200

    Done!

--- Terraformer/roadsConstruction/roads.py
+++ Terraformer/roadsConstruction/roads.py
@@ -7,11 +7,16 @@ import main
 # import Editor
 
 import random
+from random import randint
 
 from PIL import Image
 from collections import Counter
 
 alreadyGenerated = []
+
+import sys
+sys.path.append("Generator/Constructions/houses")
+import house
 
 
 ######################## Lanes materials presets #######################
@@ -573,3 +578,270 @@ standard_modern_lanes_agencement = {
         },
     },
 }
+
+def findGround(xzStart, xz):  # TODO: Change error.
+    """
+    Find the surface at xz using heightmap.
+
+    Args:
+        xzStart (tuple): Starting coordinates of the heightmap (northwest corner).
+        xz (tuple): Coordinates xz in the Minecraft world.
+
+    Returns:
+        tuple: Coordinates xyz in the Minecraft world.
+    """
+    im = Image.open("heightmap.png")
+    x = round(xz[0] - xzStart[0])
+    z = round(xz[-1] - xzStart[-1])
+    # Alpha is defined as the height ([3]).
+    width, height = im.size
+    if x >= width or z >= height:
+        print("img:", x, z)
+        print(width, height)
+        print(xzStart, xz)
+    try:
+        return xz[0], (im.getpixel((x, z))[2]) - 1, xz[-1]
+    except:
+        print("Error getpixel in map.py:42 with ", x, z)
+        return None
+
+######
+
+from gdpc import Block as place
+
+import sys
+sys.path.append("Terraformer/mapAnalysis/")
+import Skeleton 
+
+from gdpc import Editor
+
+from math import sqrt
+
+def get_distance(point1, point2):
+    # Calculate the Euclidean distance between two points
+    return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
+
+def simplify_coordinates(coordinates, epsilon):
+    if len(coordinates) < 3:
+        return coordinates
+
+    # Find the point with the maximum distance
+    max_distance = 0
+    max_index = 0
+    end_index = len(coordinates) - 1
+
+    for i in range(1, end_index):
+        distance = get_distance(coordinates[i], coordinates[0])
+        if distance > max_distance:
+            max_distance = distance
+            max_index = i
+
+    simplified_coordinates = []
+
+    # If the maximum distance is greater than epsilon, recursively simplify
+    if max_distance > epsilon:
+        rec_results1 = simplify_coordinates(coordinates[:max_index+1], epsilon)
+        rec_results2 = simplify_coordinates(coordinates[max_index:], epsilon)
+
+        # Combine the simplified sub-results
+        simplified_coordinates.extend(rec_results1[:-1])
+        simplified_coordinates.extend(rec_results2)
+    else:
+        # The maximum distance is less than epsilon, retain the endpoints
+        simplified_coordinates.append(coordinates[0])
+        simplified_coordinates.append(coordinates[end_index])
+
+    return simplified_coordinates
+
+
+def irlToMc(coordinates):
+
+    print(coordinates, "coordinates")
+    editor = Editor()
+    buildArea = editor.getBuildArea()
+    xMin = (editor.getBuildArea().begin).x
+    yMin = (editor.getBuildArea().begin).y
+    zMin = (editor.getBuildArea().begin).z
+
+    print(xMin, yMin, zMin)
+
+    coordinates_final = []
+
+    coordinates_final.append(coordinates[0] + xMin)
+    coordinates_final.append(coordinates[1] + yMin)
+    coordinates_final.append(coordinates[2] + zMin)
+
+    print(coordinates_final)
+
+    return coordinates_final
+
+def setRoads(skeleton):
+    # Generation
+    for i in range(len(skeleton.lines)):
+        for j in range(len(skeleton.lines[i])):
+            xyz = irlToMc(skeleton.coordinates[skeleton.lines[i][j]])
+            skeleton.lines[i][j] = xyz
+            print(skeleton.lines[i][j], "eee")
+
+    # Simplification
+
+    for i in range(len(skeleton.lines)):
+        skeleton.lines[i] = simplify_coordinates(skeleton.lines[i], 10)
+
+
+    for i in range(len(skeleton.lines)):  # HERE --------------------------------------
+        print(skeleton.lines[i])
+        road = RoadCurve(standard_modern_lane_agencement, skeleton.lines[i])
+        road.setLanes()
+        print(road.getLanes(), "LANES ***********")
+
+    rejected = []
+    accepted = []
+    # print(housesCoordinates)
+    for i in range(len(housesCoordinates)):
+        pos = housesCoordinates[i]
+        # print(pos, "pos0")
+        editor = Editor()
+        xMin = (editor.getBuildArea().begin).x
+        yMin = (editor.getBuildArea().begin).y
+        zMin = (editor.getBuildArea().begin).z
+        base = findGround((xMin, zMin), pos)
+        if base != None:
+            # print(pos, "pos1")
+            pos1 = (
+                pos[0] - random.randint(3, 6),
+                base[1],
+                pos[2] - random.randint(3, 6),
+            )
+            pos2 = (
+                pos[0] + random.randint(3, 6),
+                base[1],
+                pos[2] + random.randint(3, 6),
+            )
+            pos3 = (
+                pos1[0],
+                base[1],
+                pos2[2],
+            )
+            pos4 = (
+                pos2[0],
+                base[1],
+                pos1[2],
+            )
+            # print(pos1, pos2, pos3, pos4, "pos")
+            xMin = (editor.getBuildArea().begin).x
+            yMin = (editor.getBuildArea().begin).y
+            zMin = (editor.getBuildArea().begin).z
+            Ypos1 = findGround((xMin, zMin), pos1)
+            Ypos2 = findGround((xMin, zMin), pos2)
+            Ypos3 = findGround((xMin, zMin), pos3)
+            Ypos4 = findGround((xMin, zMin), pos4)
+
+            if (
+                Ypos1 != None
+                and Ypos2 != None
+                and Ypos3 != None
+                and Ypos4 != None
+            ):
+
+                pos2 = (
+                    pos2[0],
+                    max(Ypos1[1], Ypos2[1], base[1], Ypos3[1], Ypos4[1]),
+                    pos2[2],
+                )
+                pos1 = (
+                    pos1[0],
+                    max(Ypos1[1], Ypos2[1], base[1], Ypos3[1], Ypos4[1]),
+                    pos1[2],
+                )
+                if (
+                    (pos1[0], pos1[2]) not in alreadyGenerated
+                    and (
+                        pos2[0],
+                        pos2[2],
+                    )
+                    not in alreadyGenerated
+                    and (pos1[0], pos2[2]) not in alreadyGenerated
+                    and (pos2[0], pos1[2])
+                ):  # HERE, remove print and find why house gen on self
+
+                    for xi in range(
+                        -5,
+                        (max(pos1[0], pos2[0]) - min(pos1[0], pos2[0])) + 5,
+                    ):
+                        for yi in range(
+                            -5,
+                            (max(pos1[2], pos2[2]) - min(pos1[2], pos2[2]))
+                            + 5,
+                        ):
+                            alreadyGenerated.append(
+                                (
+                                    min(pos1[0], pos2[0]) + xi,
+                                    min(pos1[2], pos2[2]) + yi,
+                                )
+                            )
+
+                    nb_style=randint(0,3)
+                    door = ["left", "right"]
+                    r2 = random.randint(0, 1)
+                    facing = ["south", "north", "east", "west"]
+                    r3 = random.randint(0, 3)
+                    
+                    house.house(pos1,pos2,door[r2], 10, nb_style, facing[r3])
+                    accepted.append(
+                        (
+                            pos1[0],
+                            pos1[2],
+                            pos2[0],
+                            pos2[2],
+                        )
+                    )
+                else:
+                    rejected.append(
+                        (
+                            pos1[0],
+                            pos1[2],
+                            pos2[0],
+                            pos2[2],
+                        )
+                    )
+
+    print("Done")
+
+
+# ### Execution ###
+
+
+# from skimage.data import binary_blobs
+# blobs = binary_blobs(16, volume_fraction=0.3, n_dim=3)
+
+# # # # # # #
+
+import sys
+sys.path.append("Terraformer/dataAcquisition/")
+import World
+
+w = World.World()
+
+import sys
+sys.setrecursionlimit(w.length_x * w.length_y * w.length_z)
+
+w.propagate((1754, 67, 635)) # Change here.
+print("done")
+print(w.binaryImage())
+
+# # # # # #
+
+skel = Skeleton.Skeleton()
+
+skel.setSkeleton(w.binaryImage())
+
+skel.parseGraph()
+print(skel.centers)
+print(skel.lines)
+print(skel.intersections)
+print(skel.coordinates)
+
+Skeleton.heightmap()
+
+setRoads(skel)
